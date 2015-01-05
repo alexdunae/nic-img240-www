68a6babd648957f17d2356d6a730784c93f213e8
Alex Dunae <alex@dunae.ca>
2015-01-05 14:57:37 -0800

    Initial import

---
+++ config.rb
@@ -0,0 +1,26 @@
+set :site_title, 'NIC IMG 240'
+
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+
+activate :syntax, line_numbers: false
+
+activate :autoprefixer do |config|
+  config.browsers = ['> 1%', 'last 4 versions', 'ie 7', 'ie 8', 'ie 9', 'ff 17', 'opera 12.1']
+end
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
+end
+
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.method = :rsync
+  deploy.host = 'adunae.imgd.ca'
+  deploy.path = 'public_html'
+  deploy.user = 'adunae'
+  deploy.clean = false
+end
